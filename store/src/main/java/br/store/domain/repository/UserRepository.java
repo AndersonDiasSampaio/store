c42c2443d0f221884906176f52638dc413084351
Anderson <ads@ic.ufal.br>
2021-04-30 14:35:23 -0300

    Delete coments

--- store/src/main/java/br/store/domain/repository/UserRepository.java
+++ store/src/main/java/br/store/domain/repository/UserRepository.java
@@ -18,79 +18,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 
-/*
-@Repository
-public class Users {
-
-	@Autowired
-	private EntityManager entityManager;
-
-	private String nome;
-
-	@Transactional
-	public User salvar(User user) {
-
-		entityManager.persist(user);
-		// String insert= "insert into user set
-		// userName='"+user.getUserName()+"',userPassword='"+user.getUserPassword()+"')";
-		// String insert = "insert into user values('"+user.getId()+"',
-		// '"+user.getUserName()+"','"+user.getUserPassword()+"')";
-		// jdbcTemplate.update(insert);
-		return user;
-	}
-
-	@Transactional
-	public User update(User user) {
-		entityManager.merge(user);
-		return user;
-	}
-
-	@Transactional
-	public List<User> seachall() {
-
-		return entityManager.createQuery("from User", User.class).getResultList();
-
-	}
-	@Transactional
-	public List<User> seachByName(String nome) {
-
-		String jpql = "select c from User c where c.userName like :nome";
-		TypedQuery<User> query = entityManager.createQuery(jpql, User.class);
-		query.setParameter("nome", "%" + nome + "%");
-		return query.getResultList();
-
-	}
-	@Transactional
-	public void deletar (Integer id) {
-		
-		User user=entityManager.find( User.class, id );
-		deletar(user);
-	}
-	@Transactional
-	private void deletar(User user) {
-		 if(!entityManager.contains(user)){
-	            user = entityManager.merge(user);
-	        }
-	        entityManager.remove(user);
-		
-	}
-
-}
-*/
-
 public interface UserRepository extends JpaRepository<User, Integer > {
-	/*
-    @Query(value = " select * from User c where c.userName like '%:nome%' ", nativeQuery = true)
-    List<User> encontrarPorNome( @Param("userName") String nome );
 
-    @Query(" delete from User c where c.userName =:nome ")
-    @Modifying
-    void deleteByNome(String nome);
-
-    boolean existsByNome(String nome);
-
-    @Query(" select c from User c left join fetch c.pedidos where c.id = :id  ")
-    User findClienteFetchPedidos( @Param("id") Integer id );*/
 
 
 }
